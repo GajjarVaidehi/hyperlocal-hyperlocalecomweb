b135578d0d47e910edd9496e8b5274fb5342fee7
GajjarVaidehi <vedugajjar7101@gmail.com>
2022-03-19 20:03:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import logo from "./logo.svg";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import { Route, BrowserRouter, Routes, Navigate } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ProductInfo from "./pages/ProductInfo";
+import CartPage from "./pages/CartPage";
+
+import "./stylesheets/layout.css";
+import "./stylesheets/products.css";
+import "./stylesheets/authentication.css";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import OrdersPage from "./pages/OrdersPage";
+import AdminPage from "./pages/AdminPage";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <ToastContainer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/"
+            exact
+            element={<ProtectedRoutes><HomePage /></ProtectedRoutes>} />
+          <Route path="/productinfo/:productid"
+            exact
+            element={<ProtectedRoutes><ProductInfo /></ProtectedRoutes>}
+          />
+          <Route path="/cart"
+            exact
+            element={<ProtectedRoutes><CartPage /></ProtectedRoutes>} />
+
+          <Route path="/orders"
+            exact
+            element={<ProtectedRoutes><OrdersPage /></ProtectedRoutes>} />
+
+          <Route path="/admin"
+            exact
+            element={<ProtectedRoutes><AdminPage /></ProtectedRoutes>} />
+
+
+          <Route path="/login" exact element={<LoginPage />} />
+          <Route path="/register" exact element={<RegisterPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
+
+export const ProtectedRoutes = ({ children }) => {
+  if (localStorage.getItem('currentUser')) {
+    return children
+  }
+  else {
+    return <Navigate to='/login' />
+  }
+
+}
