f773c9dcd1a7fe82af525931f2444706bd7f8e46
GajjarVaidehi <vedugajjar7101@gmail.com>
2022-04-03 18:41:12 +0530

    commit

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -81,133 +81,140 @@ function HomePage() {
 
   return (
     <Layout loading={loading}>
-      <h2 className="mt-5 md-5 shop-class">
-        <span>&nbsp;&nbsp; Shops Near You &nbsp;&nbsp; </span>
-      </h2>
-      <div className="container">
-        <div className="row">
-          {shops.map((shop) => {
-            return (
-              <div className="col-md-4">
-                <div className="m-3 md-4 p-1 product position-relative">
-                  <div className="contents d-flex flex-column align-items-center">
-                    <div className="text-center">
-                      <img src={shop.imageURL} alt="" className="product-img" />
-                    </div>
-                    <p style={{ fontSize: "3vmin" }}>{shop.shopName}</p>
-                    <p style={{ fontSize: "2vmin" }}>{shop.shopAddress}</p>
-
-                    <div className="d-flex align-items-center">
-                      <button
-                        className="view-button "
-                        onClick={() => {
-                          navigate("/shopproducts");
-                        }}
-                      >
-                        VIEW
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-
-        <button
-          className="view-button "
-          style={{ float: "right" }}
-          onClick={() => {
-            navigate("/allshops");
-          }}
-        >
-          VIEW MORE
-        </button>
-
-        <br />
-
+      {" "}
+      <div className="bgimg">
         <h2 className="mt-5 md-5 shop-class">
-          <span>&nbsp;&nbsp; Products &nbsp;&nbsp; </span>
+          <span>&nbsp;&nbsp; Shops Near You &nbsp;&nbsp; </span>
         </h2>
-
-        <div className="d-flex w-50 align-items-center my-3 justify-content-center">
-          <input
-            type="text"
-            value={searchKey}
-            onChange={(e) => {
-              setSearchKey(e.target.value);
-            }}
-            className="form-control mx-2"
-            placeholder="'Search Items"
-          />
-          <select
-            className="form-control mt-3"
-            value={filterType}
-            onChange={(e) => {
-              setFilterType(e.target.value);
-            }}
-          >
-            <option value="">All</option>
-            <option value="sports">Sports</option>
-            <option value="toys/games">Toys/Games</option>
-            <option value="grocery">Grocery</option>
-            <option value="fashion">Fashion</option>
-            <option value="dairy">Dairy</option>
-            <option value="footwear">Footwear</option>
-            <option value="electronics">Electronics</option>
-          </select>
-        </div>
-
-        <div className="row">
-          {products
-            .filter((obj) => obj.name.toLowerCase().includes(searchKey))
-            .filter((obj) => obj.category.toLowerCase().includes(filterType))
-            .map((product) => {
+        <div className="container">
+          <div className="row">
+            {shops.map((shop) => {
               return (
                 <div className="col-md-4">
-                  <div className="m-4 p-1 product position-relative">
-                    <div className="contents">
-                      <h5>
-                        <b>{product.name}</b>
-                      </h5>
-                      <h5>{product.ownerShop}</h5>
+                  <div className="m-3 md-4 p-1 product position-relative">
+                    <div className="contents d-flex flex-column align-items-center">
                       <div className="text-center">
                         <img
-                          src={product.imageURL}
+                          src={shop.imageURL}
                           alt=""
                           className="product-img"
                         />
                       </div>
-                    </div>
-                    <div className="product-actions">
-                      <h2>Rs.{product.price} /-</h2>
-                      <div className="d-flex">
+                      <p style={{ fontSize: "3vmin" }}>{shop.shopName}</p>
+                      <p style={{ fontSize: "2vmin" }}>{shop.shopAddress}</p>
+
+                      <div className="d-flex align-items-center">
                         <button
-                          className="mx-2"
-                          onClick={() => addToCart(product)}
+                          className="view-button "
+                          onClick={() => {
+                            navigate("/shopproducts");
+                          }}
                         >
-                          ADD TO CART
-                        </button>
-                        <button
-                          className="mx-2"
-                          onClick={() => addToWishlist(product)}
-                        >
-                          ADD TO WISHLIST
+                          VIEW
                         </button>
                       </div>
-                      <button
-                        className="view-button"
-                        onClick={() => {
-                          navigate(`/productinfo/${product.id}`);
-                        }}
-                      >
-                        VIEW
-                      </button>
                     </div>
                   </div>
                 </div>
               );
             })}
+          </div>
+
+          <button
+            className="view-button "
+            style={{ float: "right" }}
+            onClick={() => {
+              navigate("/allshops");
+            }}
+          >
+            VIEW MORE
+          </button>
+
+          <br />
+
+          <h2 className="mt-5 md-5 shop-class">
+            <span>&nbsp;&nbsp; Products &nbsp;&nbsp; </span>
+          </h2>
+
+          <div className="d-flex w-50 align-items-center my-3 justify-content-center">
+            <input
+              type="text"
+              value={searchKey}
+              onChange={(e) => {
+                setSearchKey(e.target.value);
+              }}
+              className="form-control mx-2"
+              placeholder="'Search Items"
+            />
+            <select
+              className="form-control mt-3"
+              value={filterType}
+              onChange={(e) => {
+                setFilterType(e.target.value);
+              }}
+            >
+              <option value="">All</option>
+              <option value="sports">Sports</option>
+              <option value="toys/games">Toys/Games</option>
+              <option value="grocery">Grocery</option>
+              <option value="fashion">Fashion</option>
+              <option value="dairy">Dairy</option>
+              <option value="footwear">Footwear</option>
+              <option value="electronics">Electronics</option>
+            </select>
+          </div>
+
+          <div className="row">
+            {products
+              .filter((obj) => obj.name.toLowerCase().includes(searchKey))
+              .filter((obj) => obj.category.toLowerCase().includes(filterType))
+              .map((product) => {
+                return (
+                  <div className="col-md-4">
+                    <div className="m-4 p-1 product position-relative">
+                      <div className="contents">
+                        <h5>
+                          <b>{product.name}</b>
+                        </h5>
+                        <h5>{product.ownerShop}</h5>
+                        <div className="text-center">
+                          <img
+                            src={product.imageURL}
+                            alt=""
+                            className="product-img"
+                          />
+                        </div>
+                      </div>
+                      <div className="product-actions">
+                        <h2>Rs.{product.price} /-</h2>
+                        <div className="d-flex">
+                          <button
+                            className="mx-2"
+                            onClick={() => addToCart(product)}
+                          >
+                            ADD TO CART
+                          </button>
+                          <button
+                            className="mx-2"
+                            onClick={() => addToWishlist(product)}
+                          >
+                            ADD TO WISHLIST
+                          </button>
+                        </div>
+                        <button
+                          className="view-button"
+                          onClick={() => {
+                            navigate(`/productinfo/${product.id}`);
+                          }}
+                        >
+                          VIEW
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
         </div>
       </div>
     </Layout>
